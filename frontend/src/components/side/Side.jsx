44cc7efb4cef74f50bb2998b24ae59cbe06e84d7
Abdalla <anddragon34@gmail.com>
2025-06-01 19:06:32 +0300

    add Side bar

---
+++ frontend/src/components/side/Side.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Side = () => {
+  return (
+    <div className='delya'>
+        Side
+        <p>hello</p>
+        <h1>hjlahd</h1>
+        <div>
+            halhD;IshuDJ;qd;ijfe
+            <P>jilDPIJ;Dhdq</P>
+        </div>
+        </div>
+  )
+}
+
+export default Side
